46c9d93160652bcefd8cd85d6aec91afcc042a7e
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-06 15:20:51 +0100

    edited task-01 js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+let colorSwitcher;
+
+startBtn.addEventListener('click', () => {
+   colorSwitcher = setInterval(() => {
+    (getRandomHexColor)}, 1000);
+   return body.style.backgroundColor === colorSwitcher;
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(colorSwitcher);
+});
