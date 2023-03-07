0c4d3c5491ce54f7e363e3e902467f2ba6436856
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-07 15:58:54 +0100

    edited js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,15 +3,32 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 const startCountdown = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+startBtn.addEventListener('click', () => {
 
-  flatpickr(startCountdown, options);
+});
+
+const fp = flatpickr(startCountdown, () => {
+    const options = {
+        enableTime: true,
+        time_24hr: true,
+        defaultDate: new Date(),
+        minuteIncrement: 1,
+        onClose(selectedDates) {
+          console.log(selectedDates[0]);
+        },
+      };
+  });
+
+function Countdown() {
+    days = counter--;
+    hours = counter--;
+    minutes = counter--;
+    seconds = counter--;
+};
+
