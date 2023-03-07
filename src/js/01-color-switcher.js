a97c634cd2a79290517d0cd68e1b135cd21a1dfb
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-07 15:18:44 +0100

    edited const in task-1 js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,14 +6,14 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-let colorSwitcher;
+let colorTimer;
 
 startBtn.addEventListener('click', () => {
-    colorSwitcher = setInterval(() => {
+    colorTimer = setInterval(() => {
     const randomColor = getRandomHexColor(); 
     body.style.backgroundColor = randomColor;}, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
-    clearInterval(colorSwitcher);
+    clearInterval(colorTimer);
 });
