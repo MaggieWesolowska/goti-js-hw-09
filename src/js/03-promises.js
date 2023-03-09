d3f488a91d5a524adbac21a550d99f7033e536c8
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-09 15:59:18 +0100

    edited js in task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +8,10 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+createPromise(2, 1500)
+  .then(({ position, delay }) => {
+    console.log(`Notiflix.Notify.success(${position} in ${delay}ms)`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`Notiflix.Notify.failure(${position} in ${delay}ms)`);
+  });
