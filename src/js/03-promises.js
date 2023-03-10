bef064fdade8b33ed823494dd94b84211e44ae2d
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-10 15:31:49 +0100

    edited js for task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,40 +1,38 @@
 import Notiflix from 'notiflix';
 
 const form = document.querySelector('form');
-const firstDelay = document.querySelector('input[name="delay"]');
-const delayStep = document.querySelector('input[name="step"]');
-const amount = document.querySelector('input[name="amount"]');
 
-form.addEventListener('submit', (event) => {
+form.addEventListener('submit', event => {
   event.preventDefault();
   const formInput = {
-    firstDelay: firstDelay.value,
-    delayStep: delayStep.value,
-    amount: amount.value,
+    delay: Number(form.delay.value),
+    step: Number(form.step.value),
+    amount: Number(form.amount.value),
+  };
+  for (let i = 1; i <= formInput.amount; i++) {
+    createPromise(i, formInput.delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    formInput.delay += formInput.step;
   }
-  createPromise(position, delay);
 });
-
-function getFormInput() {
-  const formInput = {
-    firstDelay: firstDelay.value,
-    delayStep: delayStep.value,
-    amount: amount.value,
-  }
-}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const newPromise = new Promise(callback);
+
+  function callback(resolve, reject) {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   }
+  return newPromise;
 }
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(Notiflix.Notify.success`(${position} in ${delay}ms)`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(Notiflix.Notify.failure`(${position} in ${delay}ms)`);
-  });
