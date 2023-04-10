999463f4041e603f5e05ab19bd3b40da111719b1
MaggieWesolowska <114258920+MaggieWesolowska@users.noreply.github.com>
2023-04-10 12:31:54 +0000

    Deploying to gh-pages from @ MaggieWesolowska/goti-js-hw-09@b4cb1be0b13645412d4449d737b591d9c35e1a4d 🚀

---
+++ 01-color-switcher.30423e60.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=document.querySelector("body"),r=function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=t};e.addEventListener("click",(function(){t=setInterval(r,1e3)})),n.addEventListener("click",(function(){clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.30423e60.js.map
