b4cb1be0b13645412d4449d737b591d9c35e1a4d
MaggieWesolowska <maggiewes07@gmail.com>
2023-04-10 14:31:30 +0200

    edited js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,21 +2,21 @@ const startBtn = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 const body = document.querySelector('body');
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
 let colorTimer;
 
 const assignBodyColor = () => {
-    const randomColor = getRandomHexColor();
-    body.style.backgroundColor = randomColor;
-}
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+};
 
 startBtn.addEventListener('click', () => {
-    colorTimer = setInterval(assignBodyColor, 1000);
+  colorTimer = setInterval(assignBodyColor, 1000);
 });
 
 stopBtn.addEventListener('click', () => {
-    clearInterval(colorTimer);
+  clearInterval(colorTimer);
 });
